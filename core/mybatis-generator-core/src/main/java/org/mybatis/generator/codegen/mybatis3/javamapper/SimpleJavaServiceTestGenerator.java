cd20aa99e693dbfc46ed40137ee2982f429b576b
yufeng0528 <yufeng0528@126.com>
2016-09-07 21:27:00 +0800

    ServiceTest done

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/javamapper/SimpleJavaServiceTestGenerator.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/javamapper/SimpleJavaServiceTestGenerator.java
@@ -15,6 +15,7 @@
  */
 package org.mybatis.generator.codegen.mybatis3.javamapper;
 
+import static org.mybatis.generator.internal.util.JavaBeansUtil.getJavaBeansGetter;
 import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.CompilationUnit;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
@@ -55,6 +57,14 @@ public class SimpleJavaServiceTestGenerator extends AbstractJavaClientGenerator 
     			myBatis3JavaServiceType.substring(start + 1, myBatis3JavaServiceType.length());
     }
     
+    private String getDomainName(){
+    	String domanName = introspectedTable.getBaseRecordType();
+    	if(domanName.indexOf(".") == -1)
+    		return domanName;
+    	int start = domanName.lastIndexOf(".") + 1;
+    	return domanName.substring(start, domanName.length());
+    }
+    
     @Override
     public List<CompilationUnit> getCompilationUnits() {
     	Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
@@ -81,6 +91,8 @@ public class SimpleJavaServiceTestGenerator extends AbstractJavaClientGenerator 
         topLevelClass.addField(field);
         
         
+        importedTypes.add(new FullyQualifiedJavaType("org.junit.Test"));
+        addGenModelMethod(topLevelClass, importedTypes);
         addInsertMethood(topLevelClass, importedTypes);
         addUpdateByPrimaryKeyMethod(topLevelClass, importedTypes);
         
@@ -95,6 +107,61 @@ public class SimpleJavaServiceTestGenerator extends AbstractJavaClientGenerator 
         return answer;
     }
     
+    protected void addGenModelMethod(TopLevelClass topLevelClass, Set<FullyQualifiedJavaType> importedTypes) {
+    	Method method = new Method();
+        method.setVisibility(JavaVisibility.PRIVATE);
+        method.setName("genRecord"); //$NON-NLS-1$
+        
+        FullyQualifiedJavaType type = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        importedTypes.add(type);
+        
+        method.addBodyLine(getDomainName() + " record = new " + getDomainName() + "();");
+        method.addBodyLine("//TODO:");
+        method.addBodyLine("return record;"); 
+        topLevelClass.addMethod(method);
+    }
+    
+    private String getGetterMethodName(Set<FullyQualifiedJavaType> importedTypes){
+    	StringBuilder sb = new StringBuilder();
+    	
+    	List<IntrospectedColumn> introspectedColumns = introspectedTable
+                .getPrimaryKeyColumns();
+    	for (IntrospectedColumn introspectedColumn : introspectedColumns) {
+            FullyQualifiedJavaType type = introspectedColumn
+                    .getFullyQualifiedJavaType();
+            importedTypes.add(type);
+            
+            sb.append(",");
+            sb.append(getJavaBeansGetter(introspectedColumn, context, introspectedTable).getName());
+            sb.append("()");
+        }
+        sb.deleteCharAt(0);
+    	
+    	return sb.toString();
+    }
+    
+    private void getAssertMethod(Method method, Set<FullyQualifiedJavaType> importedTypes){
+    	StringBuilder sb = new StringBuilder();
+    	
+    	List<IntrospectedColumn> introspectedColumns = introspectedTable
+                .getAllColumns();
+    	for (IntrospectedColumn introspectedColumn : introspectedColumns) {
+            FullyQualifiedJavaType type = introspectedColumn
+                    .getFullyQualifiedJavaType();
+            importedTypes.add(type);
+            
+            sb.setLength(0);
+            sb.append("assertEquals(record.");
+            sb.append(getJavaBeansGetter(introspectedColumn, context, introspectedTable).getName());
+            sb.append("(), ");
+            sb.append("recordInDb.");
+            sb.append(getJavaBeansGetter(introspectedColumn, context, introspectedTable).getName());
+            sb.append("());");
+            method.addBodyLine(sb.toString());
+        }
+    	
+    }
+    
     protected void addInsertMethood(TopLevelClass topLevelClass, Set<FullyQualifiedJavaType> importedTypes) {
         if (introspectedTable.getRules().generateSelectByPrimaryKey()) {
             Method method = new Method();
@@ -104,8 +171,17 @@ public class SimpleJavaServiceTestGenerator extends AbstractJavaClientGenerator 
             
             FullyQualifiedJavaType type = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
             importedTypes.add(type);
-            method.addParameter(new Parameter(type, "record"));
-            method.addBodyLine(getServiceName() + ".insert(record);"); 
+            method.addBodyLine(getDomainName() + " record = genRecord();");
+            method.addBodyLine(getServiceName() + ".insert(record);");
+            
+            method.addBodyLine("");
+            
+            method.addBodyLine(getDomainName() + " recordInDb = " + 
+            		getServiceName() + ".getById(record." + getGetterMethodName(importedTypes) + ")");
+            
+            //add assert 
+            getAssertMethod(method, importedTypes);
+            
             topLevelClass.addMethod(method);
         }
     }
